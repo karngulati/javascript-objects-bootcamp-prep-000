f83eb1f298c4718a2c290873b7e627fa50a9e711
Karn Gulati <karngulati@gmail.com>
2019-02-26 22:44:51 +0000

    Automatically backed up by Learn

--- objects.js
+++ objects.js
@@ -6,6 +6,6 @@ function updatePlaylist(playlist, artistName, songTitle) {
 }
 
 function removeFromPlaylist(playlist, artistName, songTitle) {
- delete playlist[artistName] = songTitle
+ delete playlist[artistName]
   return playlist
 }
